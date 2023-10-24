83c38c4c423507f0940fff89e60e5536f0a9cdf3
ignasiFloz <gurreaignasi@gmail.com>
2023-10-24 11:39:59 +0200

    los tests funcionan, la cli no, debido a problemas con imports

---
+++ src/cli.js
@@ -0,0 +1,22 @@
+
+import { Command } from 'commander';
+import { debounce } from './fncDebounce.js'; // Importa la función debounce desde tu archivo fncDebounce.ts
+const program = new Command();
+program
+    .version('1.0.0')
+    .description('CLI para simular una acción con debounce en TypeScript');
+program
+    .command('action <delay> <message>')
+    .description('Ejecutar una acción después de un retraso personalizado')
+    .action((delay, message) => {
+    const delayMs = parseInt(delay);
+    const debouncedAction = debounce(() => {
+        console.log(`¡Acción ejecutada: ${message}`);
+    }, delayMs);
+    console.log(`Esperando ${delayMs} milisegundos antes de ejecutar la acción...`);
+    // Iniciar el retraso y la acción
+    debouncedAction();
+});
+program.parse(process.argv);
+//node .\cli.js  action 3000 "Mensaje de prueba"
+//node .\cli.js
